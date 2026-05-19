c58ddb092ab72573ad0d1863e3c7b850bb9e24ac
DaafH <Daafheijnekamp@gmail.com>
2026-05-19 10:53:49 +0200

    Clean Version

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-const DigitalThankYouWall = dynamic(() => import('@/components/DigitalThankYouWall'), {
+const DigitalThankYouWall = dynamic(() => import('@/components/digital-thank-you-wall'), {
   ssr: false,
 });
 
